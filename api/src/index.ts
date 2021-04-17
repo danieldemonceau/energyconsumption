7826c1b83619becfb45443e3f93bc01300f574bb
Daniel Demonceau <daniel.demonceau@yahoo.com>
2021-04-17 16:12:01 +1000

    http -> https

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,19 @@
+import https from 'https';
+// import http from 'http';
+import fs from 'fs';
 import app from './app';
 import { APP_ENV, APP_PORT } from './config';
+
 // import logger from './logger';
 
-app.listen(APP_PORT);
+const options = {
+  key: fs.readFileSync(`${__dirname} /../key.pem`),
+  cert: fs.readFileSync(`${__dirname} /../cert.pem`),
+};
+
+/* app.listen(APP_PORT);
+   http.createServer(app).listen(APP_PORT); */
+https.createServer(options, app).listen(APP_PORT);
 // .on('listening', () => logger.info(`Server running in ${APP_ENV} on port ${APP_PORT}`))
 
 console.log(`Server running in ${APP_ENV} on port ${APP_PORT}`);
