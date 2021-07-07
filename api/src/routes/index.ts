cf88f53d9638d2f77009405fd06ed9ff8abfb5fe
Daniel Demonceau <daniel.demonceau@yahoo.com>
2021-07-08 00:33:23 +1000

    Add apikey to / path

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -4,20 +4,30 @@ import usages from './usages';
 import noRouteErrorResponse from './noRouteErrorResponse';
 import httpResponse from '../httpMessages';
 // import logger from '../logger';
+import isAPIKeyValid from '../middleware/isAPIKeyValid';
 
 const routes = (app: Application): void => {
-  const getRoot = (req: Request, res: Response, next: NextFunction) => {
+  const getRoot = async (req: Request, res: Response, next: NextFunction) => {
     // logger.info(req.originalUrl);
-    const query = 'SELECT NOW()';
-    pool.query(query, (err: any, results: any) => {
-      if (err) {
-        // logger.info(req.method + ' ' + req.originalUrl + ' → ' + 'HTTP 400');
-        httpResponse(req, res, 'error', 400, `Error with query: ${query}`, err.message);
-        next(err);
-      }
-      // logger.info(req.method + ' ' + req.originalUrl + ' → ' + 'HTTP 200');
-      httpResponse(req, res, 'success', 200, `Success`, JSON.stringify(results.rows));
-    });
+    const apiKeyClient = String(req.query.apikey);
+    try {
+      await isAPIKeyValid(apiKeyClient);
+      const query = 'SELECT NOW()';
+      pool.query(query, (err: any, results: any) => {
+        if (err) {
+          // logger.info(req.method + ' ' + req.originalUrl + ' → ' + 'HTTP 400');
+          httpResponse(req, res, 'error', 400, `Error with query: ${query}`, err.message);
+          next(err);
+        }
+        // logger.info(req.method + ' ' + req.originalUrl + ' → ' + 'HTTP 200');
+        httpResponse(req, res, 'success', 200, `Success`, JSON.stringify(results.rows));
+      });
+    } catch (err) {
+      // logger.error('API key is not valid');
+      httpResponse(req, res, 'error', 400, 'Cannot get /', err.message);
+    } finally {
+      // pool.end(() => {});
+    }
   };
   app.get('/', getRoot);
   app.use('/usages', usages);
